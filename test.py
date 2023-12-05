fbfb4f95d787133bf4b8325822ff8b484299981c
EC2 Default User <ec2-user@ip-172-31-43-138.eu-north-1.compute.internal>
2023-12-05 16:00:46 +0000

    Render Simple Html file

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello")
+import boto3
+
+
+s3 = boto3.resource('s3') # creating a s3 resource
+
+bucket = s3.Bucket('dasun-bucket')
+
+object_summary_iterator = bucket.objects.all()
+
+for obj in object_summary_iterator:
+    print(obj.key)
